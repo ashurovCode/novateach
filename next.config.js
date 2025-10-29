90178ae8172896f68d1e2b9582e98abf937cc3ad
ashurovCode <isfandiyora49@gmail.com>
2025-10-29 17:14:01 +0700

    chore: update Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/novateach' : '',
-  assetPrefix: isProd ? '/novateach/' : '',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
