770b5f40edafc02bcb03d65ad8094a8b6b6e43d9
ashurovCode <isfandiyora49@gmail.com>
2025-10-29 17:16:00 +0700

    chore: add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  basePath: isProd ? '/novateach' : '',
+  assetPrefix: isProd ? '/novateach/' : '',
 }
 
 module.exports = nextConfig
